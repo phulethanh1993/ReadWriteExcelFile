bcab251d89c76bddab10c438657761efd1c38da8
Lê Phú <phubi1103@gmail.com>
2016-11-03 11:27:47 +0700

    Initialization JLabel

--- ReadWriteExcelFile/src/com/phulethanh/view/BookManager.java
+++ ReadWriteExcelFile/src/com/phulethanh/view/BookManager.java
@@ -1,7 +1,25 @@
 package com.phulethanh.view;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class BookManager extends JPanel{
-
+public class BookManager extends JPanel {
+    private JLabel lbTittle;
+    private JLabel lbBookCode;
+    private JLabel lbBookName;
+    private JLabel lbPublishYear;
+    private JLabel lbNumberOfPage;
+    private JLabel lbAuthor;
+    private JLabel lbPublishCompany;
+    private JLabel lbPrice;
+    private JLabel lbISBN;
+    private JTextField tfBookCode;
+    private JTextField tfBookName;
+    private JTextField tfPublishYear;
+    private JTextField tfNumberOfPage;
+    private JTextField tfAuthor;
+    private JTextField tfPublishCompany;
+    private JTextField tfPrice;
+    private JTextField tfÍBN;
 }
